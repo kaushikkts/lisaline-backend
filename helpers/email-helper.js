53db7d9b9842ff4de41a5cd93e74c368b093cc05
kaushikkts <105159350+kaushikkts@users.noreply.github.com>
2024-09-13 06:41:07 +0530

    Full functionality

--- helpers/email-helper.js
+++ helpers/email-helper.js
@@ -1,25 +1,3 @@
-// const formData = require('form-data');
-// const Mailgun = require('mailgun.js');
-// const mailgun = new Mailgun(formData);
-// const mg = mailgun.client({username: 'api', key: 'd35e619e0c7900a44580d4069e194325-826eddfb-7aa84d50'});
-//
-//
-// const sendEmail = async (to, zip) => {
-//     try {
-//         const result = await mg.messages.create('sandboxb792fb3c6131472588a185151e7c2acc.mailgun.org', {
-//             from: 'admin@karandikartechsolutions.com',
-//             to: to,
-//             subject: 'Certificate of Calibration',
-//             text: 'Please find the attached certificate of calibration',
-//             attachment: {filename: 'certificate.zip', data: zip, contentType: 'application/zip'},
-//         });
-//         console.log(result);
-//     } catch (e) {
-//         console.log(e);
-//     }
-// }
-//
-
 const nodemailer = require('nodemailer');
 const sendEmail = async (to, zip) => {
     let configOptions = {
